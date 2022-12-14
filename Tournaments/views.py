4463133490973fae6644216428db8b0bcd4f156b
ngdream <nguewofoss@gmail.com>
2022-12-14 12:14:17 +0100

    tis comming
    bash: ligne 1: push : commande introuvable
    ok

--- Tournaments/views.py
+++ Tournaments/views.py
@@ -1,8 +1,45 @@
 from django.shortcuts import render
 from .models import Tournament
+<<<<<<< HEAD
 
 # Create your views here.
 
 def home_view(request):
     tournaments=Tournament.objects.all()
     return render(request,"home.html")
+=======
+from django.views.generic import TemplateView
+from django.views import View
+from django.views.generic.edit import FormView
+# Create your views here.
+
+
+
+
+# some_app/views.py
+
+
+class AboutView(TemplateView):
+    template_name = "about.html"
+    
+    
+
+class HomeView(View):
+    template_name = 'form_template.html'
+    def get(self, request, *args, **kwargs):
+        return  render(request,self.template_name)
+    
+
+
+
+class AddView(FormView):
+    template_name = 'add_tournament.html'
+    form_class = AddTournamentForm
+
+    def form_valid(self, form):
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+        form.send_email()
+        return super().form_valid(form)
+
+>>>>>>> 24d2046 (is comming)
--- usjtournament/urls.py
+++ usjtournament/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('',include('Tournaments.urls')),
+    path('staff/',include('staff.urls')),
+       path('candidate',include('candidate.urls')),
+    
+
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
