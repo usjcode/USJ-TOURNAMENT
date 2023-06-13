4b559b6c70c1b504ece2ea48c1c1b07407da189a
Le-positif-clinton <105806913+Le-positif-clinton@users.noreply.github.com>
2023-06-13 11:20:37 +0100

    reset password

--- staff/urls.py
+++ staff/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='password_reset'),
     path('password_reset_send/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_send.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_form.html'), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_done.html'), name='password_reset_complete'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_done.html'), name='password_reset_complete')
 
 
 ]
